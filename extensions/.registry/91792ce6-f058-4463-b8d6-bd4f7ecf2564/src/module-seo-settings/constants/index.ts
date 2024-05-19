ba15ae28daafe0247dca541e0ac6e75fd169c19b
sazzad47 <sazzad47.ju@gmail.com>
2024-05-19 20:13:53 +0530

    first commit

---
+++ extensions/.registry/91792ce6-f058-4463-b8d6-bd4f7ecf2564/src/module-seo-settings/constants/index.ts
@@ -0,0 +1,50 @@
+export const navigatorItems = [
+    {
+        icon: 'public',
+        name: "General",
+        to: `/seo-settings`,
+        childrens: [
+            {
+                icon: 'public',
+                name: "Site basics",
+                to: `/seo-settings/site-basic`,
+            },
+            {
+                icon: 'public',
+                name: "Title & Meta",
+                to: `/seo-settings/title-meta`,
+            },
+            {
+                icon: 'public',
+                name: "Local SEO",
+                to: `/seo-settings/local-seo`,
+                // hidden: true,
+            },
+        ]
+    },
+    {
+        icon: 'manage_accounts',
+        name: "Advanced",
+        to: `/seo-settings/advanced`,
+        childrens: [
+            {
+                icon: 'public',
+                name: "Sitemap",
+                to: `/seo-settings/advanced/sitemap`,
+                hidden: true,
+            },
+            {
+                icon: 'public',
+                name: "Scripts",
+                to: `/seo-settings/advanced/scripts`,
+            },
+
+        ]
+    },
+    {
+        icon: 'start',
+        name: "Redirection",
+        to: `/seo-settings/redirection`,
+        hidden: true
+    }
+]
---
+++ extensions/.registry/91792ce6-f058-4463-b8d6-bd4f7ecf2564/src/shared/constants/index.ts
@@ -0,0 +1,11 @@
+export const COLLECTION = {
+    seo_setting: 'seo_setting',
+    seo_advanced: 'seo_advanced_setting',
+    seo_detail: 'seo_detail',
+    seo_redirection: 'seo_redirection',
+    language: 'languages',
+}
+
+export const FIELDS = {
+    seo_detail: 'seo_detail'
+}
